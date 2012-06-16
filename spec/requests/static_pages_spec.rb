6ff69a35ebcf81aeb88690e7a132e638dca3c501
tak <xilitol@hanmail.net>
2012-06-16 10:20:56 +0900

    Initial commit

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+	
+	describe "Home page" do
+
+		it "should have the h1 'Sample App' " do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Sample App')
+		end
+
+		it "should have the base title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => ' | Home')
+		end
+	end
+	
+	describe "Help page" do
+
+		it "should have the h1 'Help' " do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
+
+		it "should have the base title" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/help'
+			page.should_not have_selector('title', :text => ' | Help')
+		end
+	end
+
+	describe "About page" do
+
+		it "should have the h1 'About us' " do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About Us')
+		end
+
+		it "should have the title 'About us' " do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/about'
+			page.should_not have_selector('title', :text => ' | About')
+		end
+	end
+end
